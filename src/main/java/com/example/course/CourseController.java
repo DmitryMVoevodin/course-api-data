06fe133428c73e238a15db6d3d8bd820b4d637d5
Dmitry Voyevodin <dmvoyevodin@mail.ru>
2019-05-15 09:57:46 +0300

    Initial commit

---
+++ src/main/java/com/example/course/CourseController.java
@@ -0,0 +1,42 @@
+package com.example.course;
+
+import com.example.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/topics/{id}/courses")
+    public List<Course> getAllTopics(@PathVariable String id){
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping("/topics/{topicId}/courses/{id}")
+    public Course getCourse(@PathVariable String id){
+        return courseService.getCourse(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId){
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String id) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.update(course, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{id}")
+    public void deleteCourse(@PathVariable String topicId, @PathVariable String id) {
+        courseService.deleteCourse(id);
+    }
+
+}
